93cc9791b722f3eb8655fbf993280b06591a315e
Kimmo Palosaari <kpalosaa@gmail.com>
2016-04-16 16:11:23 +0300

    Added iOS renderer

--- Gwen.Renderer.Android.OpenTK/Properties/AssemblyInfo.cs
+++ Gwen.Renderer.Android.OpenTK/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Gwen.Renderer.Android.OpenTK")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016 Kimmo Palosaari")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
